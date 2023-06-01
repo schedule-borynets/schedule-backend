cb372283bb0bfafccfc56e85b02d5dc452b4d3e9
Sasha <bellalex02@gmail.com>
2023-06-02 01:28:59 +0300

    fix: cors origin

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ credentials: true, origin: 'http://localhost:3000' });
+  app.enableCors({
+    credentials: true,
+    origin:
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:3000'
+        : 'https://schedule-frontend-gamma.vercel.app',
+  });
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
